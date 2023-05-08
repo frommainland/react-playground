cb0808790ace5c8724d3efd67628481c4d48f59a
shuo <caihehuang@gmail.com>
2023-05-08 23:55:43 +0800

    add - sidebar styles

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,34 +1,58 @@
 import React, { useState } from 'react'
 import './Sidebar.scss'
 import { motion } from 'framer-motion'
+import { useMeasure } from 'react-use'
 
 const menuItems = [
 	'emoji on mouseover',
 	'pinterest long press',
 	'scroll velocity',
-	'animation based on time',
+	'animation based on time animation based on time',
 	'scroll speed inView',
 ]
 
-const MenuItem = ({ text, selected, onClick }) => (
-	<motion.div
-		className="menu-item"
-		onClick={onClick}
-		animate={{ opacity: selected ? 1 : 0.5 }}
-	>
-		{text}
-		{selected && <motion.div className="underline" layoutId="underline" />}
-	</motion.div>
-)
+const MenuItem = ({ text, selected, onClick }) => {
+	const [ref, { width, height }] = useMeasure()
+	return (
+		<motion.li
+			ref={ref}
+			className="menu-item"
+			onClick={onClick}
+			animate={{
+				color: selected ? '#EFFFE8' : '#3D4838',
+			}}
+		>
+			{text}
+			{selected && (
+				<motion.div
+					className="underline"
+					layoutId="underline"
+					style={{
+						width: width + 10,
+						height: height + 20,
+						top: 0,
+						left: 0,
+					}}
+				/>
+			)}
+		</motion.li>
+	)
+}
 
 const Sidebar = () => {
 	const [selected, setSelected] = useState(0)
+
 	return (
 		<nav className="sidebar">
 			<ol>
-				{menuItems.map((value, index) => {
-					return <MenuItem text={value} key={value} selected={selected}
-				})}
+				{menuItems.map((value, index) => (
+					<MenuItem
+						text={value}
+						key={value}
+						selected={selected === index}
+						onClick={() => setSelected(index)}
+					/>
+				))}
 			</ol>
 		</nav>
 	)
