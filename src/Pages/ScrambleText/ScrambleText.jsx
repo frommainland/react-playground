48f0fa5047cbc5e70c3599bab7c9096d4786b9dd
shuo <caihehuang@gmail.com>
2023-06-08 16:25:20 +0800

    test

--- src/Pages/ScrambleText/ScrambleText.jsx
+++ src/Pages/ScrambleText/ScrambleText.jsx
@@ -53,6 +53,47 @@ const emojis = [
 	'😀, 😃, 😄, 😁, 😆, 😅, 😂, 🤣, 😊, 😇, 🙂, 🙃, 😉, 😌, 😍, 🥰, 😘, 😗, 😙, 😚, 😋, 😛, 😜, 🤪, 😝, 🤑, 🤗',
 ]
 
+const blockCharacters = [
+	'\u2591',
+	'\u2592',
+	'\u2593',
+	'\u2588',
+	'\u2596',
+	'\u2597',
+	'\u2598',
+	'\u2599',
+	'\u259A',
+	'\u259B',
+	'\u259C',
+	'\u259D',
+	'\u259E',
+	'\u259F',
+]
+
+const specialCharacters = [
+	'!',
+	'§',
+	'$',
+	'%',
+	'&',
+	'/',
+	'(',
+	')',
+	'=',
+	'?',
+	'_',
+	'<',
+	'>',
+	'^',
+	'°',
+	'*',
+	'#',
+	'-',
+	':',
+	';',
+	'~',
+]
+
 const random = (min, max) => Math.floor(Math.random() * (max - min)) + min
 const range = (start, end, step = 1) => {
 	let output = []
@@ -79,13 +120,50 @@ function generateTextData(number) {
 }
 
 //
-const generateTimes = 10
+const generateTimes = 5
 const numberOfDisplayText = range(0, displayText.length)
 const displayTextArray = []
 numberOfDisplayText.map((_, index) => {
 	const scrambleTexts = generateTextData(generateTimes)
 	displayTextArray.push([...scrambleTexts, arrayOfDisplayText[index]])
 })
+
+const ScrambleTextEffect1 = () => {
+	const [text, setText] = useState(displayTextArray)
+
+	const moveY = Array.from(
+		{ length: generateTimes + 2 },
+		(_, index) => `${100 - 100 * index}%`
+	)
+
+	return (
+		<PlayGround>
+			{text.map((innerArray, i) => {
+				return (
+					<Wrapper key={i}>
+						{innerArray.map((value, index) => {
+							return (
+								<Item
+									animate={{
+										y: moveY,
+									}}
+									transition={{
+										duration: text.length / 20,
+										ease: smooth,
+										delay: 0.25 * i,
+									}}
+									key={index}
+								>
+									{value}
+								</Item>
+							)
+						})}
+					</Wrapper>
+				)
+			})}
+		</PlayGround>
+	)
+}
 
 // hugo costa https://codepen.io/hug0hq/pen/OJXXLyB
 function shuffle(word) {
@@ -115,43 +193,21 @@ const ScrambleTextEffect2 = ({ text }) => {
 		() => {
 			setActiveText(activeText + 1)
 		},
-		activeText == textArray.length - 1 ? null : 100
+		activeText == textArray.length - 1 ? null : 50
 	)
 
 	return (
 		<>
-			<h1>{textArray[activeText]}</h1>
+			<Item>{textArray[activeText]}</Item>
 		</>
 	)
 }
 
 const ScrambleTextEffect3 = () => {
 	const [text, setText] = useState('what a nice day')
-	const specialCharacters = [
-		'!',
-		'§',
-		'$',
-		'%',
-		'&',
-		'/',
-		'(',
-		')',
-		'=',
-		'?',
-		'_',
-		'<',
-		'>',
-		'^',
-		'°',
-		'*',
-		'#',
-		'-',
-		':',
-		';',
-		'~',
-	]
+
 	const random = (min, max) => Math.floor(Math.random() * (max - min)) + min
-	const time = 280
+	const time = 100
 	let count = 0
 	let j = 0
 	let delayAnimation = 0
@@ -162,10 +218,8 @@ const ScrambleTextEffect3 = () => {
 
 	const startScrambleText = () => {
 		const stringSplit = text.split('')
-		console.log(stringSplit)
 		const interval = setInterval(() => {
 			let newStringSplit = ''
-
 			for (let i = 0; i <= stringSplit.length - 1; i++) {
 				if (i <= j && count >= stringSplit.length + delayAnimation) {
 					newStringSplit += stringSplit[i]
@@ -177,7 +231,11 @@ const ScrambleTextEffect3 = () => {
 				}
 			}
 
+			// if (count === 1) {
+			// 	console.log(newStringSplit) // Log the initial value of text on the first interval
+			// }
 			setText(newStringSplit)
+
 			count++
 
 			if (count >= stringSplit.length + delayAnimation) {
@@ -190,116 +248,133 @@ const ScrambleTextEffect3 = () => {
 		}, time)
 	}
 
+	return <Item>{text}</Item>
+}
+
+function generateRandomString(inputString) {
+	const length = inputString.length
+	let randomString = ''
+
+	for (let i = 0; i < length; i++) {
+		const randomCharCode = Math.floor(Math.random() * 26) + 97 // Random lowercase character code (97-122)
+		const randomChar = String.fromCharCode(randomCharCode)
+		randomString += randomChar
+	}
+
+	return randomString
+}
+
+const ScrambleTextEffect4 = () => {
+	let max = 3
+	// const [text, setText] = useState('what a nice day')
+	const [text, setText] = useState([
+		'1',
+		'sa',
+		'dse',
+		'wih2',
+		'whza',
+		'wha6',
+		'what',
+	])
+	let temp = []
+	const [count, setCount] = useState(0)
+	const [j, setJ] = useState(0)
+
+	const [activeText, setActiveText] = useState(0)
+
+	useInterval(
+		() => {
+			if (count < max) {
+				arrayOfDisplayText.map((value, index) => {
+					temp.push(
+						generateRandomString(
+							arrayOfDisplayText.slice(0, index + 1).join('')
+						)
+					)
+				})
+			} else {
+				arrayOfDisplayText.map((value, index) => {
+					if (index <= j) {
+						temp.push(
+							arrayOfDisplayText.slice(0, index + 1).join('')
+						)
+					} else {
+						temp.push(
+							generateRandomString(
+								arrayOfDisplayText.slice(0, index + 1).join('')
+							)
+						)
+					}
+				})
+			}
+
+			setCount((prevCount) => prevCount + 1)
+			setText(temp)
+			console.log(temp)
+			if (count >= max) {
+				setJ(j + 1)
+			}
+			setActiveText(activeText + 1)
+		},
+		j == text.length ? null : 500
+	)
+
 	return (
-		<h1
-			className="text_box"
-			style={{
-				width: `${text.length}ch`,
-				animation: `typing ${(time * text.length) / 1000}s steps(${
-					text.length
-				})`,
-			}}
-		>
-			{text}
-		</h1>
+		<>
+			<Item>{text[0]}</Item>
+		</>
 	)
 }
 
 const ScrambleText = () => {
 	const [text, setText] = useState(displayTextArray)
-	// let blockCharacters = [
-	// 	'&#x2591;',
-	// 	'&#x2592;',
-	// 	'&#x2593;',
-	// 	'&#x2588;',
-	// 	'&#x2596;',
-	// 	'&#x2597;',
-	// 	'&#x2598;',
-	// 	'&#x2599;',
-	// 	'&#x259A;',
-	// 	'&#x259B;',
-	// 	'&#x259C;',
-	// 	'&#x259D;',
-	// 	'&#x259E;',
-	// 	'&#x259F;',
-	// ]
 
-    const blockCharacters = [
-        '\u2591',
-        '\u2592',
-        '\u2593',
-        '\u2588',
-        '\u2596',
-        '\u2597',
-        '\u2598',
-        '\u2599',
-        '\u259A',
-        '\u259B',
-        '\u259C',
-        '\u259D',
-        '\u259E',
-        '\u259F',
-      ];
 	const moveY = Array.from(
 		{ length: text.length - 3 },
 		(_, index) => `${100 - 100 * index}%`
 	)
 
 	return (
-		<>
-			<PlayGround>
-				{text.map((innerArray, i) => {
-					return (
-						<Wrapper key={i}>
-							{innerArray.map((value, index) => {
-								return (
-									<Item
-										animate={{
-											y: moveY,
-										}}
-										transition={{
-											duration: text.length / 10,
-											ease: smooth,
-											delay: 1 + 0.25 * i,
-										}}
-										key={index}
-									>
-										{value}
-									</Item>
-								)
-							})}
-						</Wrapper>
-					)
-				})}
-			</PlayGround>
-			<ScrambleTextEffect2 text={displayText} />
-			<ScrambleTextEffect3 />
-            <h1>{blockCharacters}</h1>
-		</>
+		<Examples>
+			<div>
+				<ScrambleTextEffect2 text={displayText} />
+				<ScrambleTextEffect3 />
+				<ScrambleTextEffect4 />
+				<ScrambleTextEffect1 />
+			</div>
+		</Examples>
 	)
 }
 
 export default ScrambleText
 
+const Examples = styled.div`
+	display: flex;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	height: 100%;
+`
+
 const PlayGround = styled.div`
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	/* height: 100%; */
 	width: 100%;
 `
 
 const Wrapper = styled.div`
 	overflow: hidden;
 	height: 32px;
-	width: 22px;
+	width: 20px;
 	text-align: center;
 `
 const Item = styled(motion.h1)`
 	line-height: 1;
 	font-size: 32px;
 	font-family: var(--apercu-bold);
+	color: var(--green3);
 `
-
 
 // https://codepen.io/anthonyChaussin/pen/xQxdNz?editors=1010
